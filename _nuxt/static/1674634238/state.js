e40ac9dc68223fc0bcc4defccb9d0df89e3fb0e1
huynamboz <teahuynam2003@gmail.com>
2023-01-27 00:38:01 +0700

    Updates

--- _nuxt/static/1674634238/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbox-phim\u002F_nuxt\u002Fstatic\u002F1674634238",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fbox-phim\u002F",assetsPath:"\u002Fbox-phim\u002F_nuxt\u002F",cdnURL:a}}}}(null));
